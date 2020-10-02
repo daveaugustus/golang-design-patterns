6d4556a744da6783f31e0e5c4de078131664c6b1
Dave Augustus <davetweetlive@gmail.com>
2020-10-02 23:59:16 +0530

    Register feature working

--- main.go
+++ main.go
@@ -1,16 +1,49 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/davetweetlive/offersapp/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 )
 
 func main() {
+
+	conn, err := connectDB()
+	if err != nil {
+		return
+	}
+
 	router := gin.Default()
-	userGroup := router.Group("users")
+
+	router.Use(dbMiddleware(*conn))
+
+	usersGroup := router.Group("users")
+	{
+		usersGroup.POST("register", routes.UserRegister)
+	}
 
 	router.Run(":8000")
 
-	fmt.Println(userGroup)
+}
+
+func connectDB() (c *pgx.Conn, err error) {
+	conn, err := pgx.Connect(context.Background(), `postgres://buxolvcz:3Tw8-FCDL4qHPO0Vicshej9fFGcH1v6c@lallah.db.elephantsql.com:5432/buxolvcz`)
+	if err != nil {
+		fmt.Println("Error connecting to DB")
+		fmt.Println(err.Error())
+	}
+	if err := conn.Ping(context.Background()); err != nil {
+		fmt.Println("Internal server error!")
+	}
+	return conn, err
+}
+
+func dbMiddleware(conn pgx.Conn) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("db", conn)
+		c.Next()
+	}
 }
---
+++ routes/users.go
@@ -0,0 +1,37 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/davetweetlive/offersapp/models"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
+)
+
+// UserRegister asd
+func UserRegister(c *gin.Context) {
+	user := models.User{}
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db, _ := c.Get("db")
+	conn := db.(pgx.Conn)
+	if err := user.Register(&conn); err != nil {
+		fmt.Println("Error in user.Register()")
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	token, err := user.GetAuthToken()
+	if err == nil {
+		c.JSON(http.StatusOK, gin.H{
+			"token": token,
+		})
+		return
+	}
+	// c.JSON(http.StatusCreated, gin.H{"user_id": user.ID})
+	// c.JSON(http.StatusCreated, gin.H{"error": "There was an error authenticating."})
+}
